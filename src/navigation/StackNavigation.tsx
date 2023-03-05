3aeebd07aa5da2ae42c12056fabcbfc390250006
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2023-03-05 17:28:22 +0530

    init pages

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import Place from '../screens/Place';
+
+const Stack = createNativeStackNavigator();
+
+const AppStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Place" component={Place} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppStack;
